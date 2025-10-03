8af32e83115f9982d9dea0b2cbb1cd29f5ba02ea
Александр Бердышев <alex_berdyshev29@mail.ru>
2025-10-04 00:41:59 +0500

    Больше функций

---
+++ js/functions.js
@@ -0,0 +1,20 @@
+//первая функция
+function checkStringLength (ourString, maxLength) {
+  if (ourString.length <= maxLength)
+    return true;
+  return false;
+};
+checkStringLength('лунтик', 20);
+checkStringLength('уга буга', 2);
+checkStringLength('пустой лес', 10);
+
+//вторая функция
+function isPalindrome (firstString) {
+  const secondString = firstString.replaceAll(' ', '').toLowerCase();
+  const reverseString = secondString.split('').reverse().join('');
+  if (secondString === reverseString) return true;
+  else return false;
+};
+isPalindrome('топот');
+isPalindrome('ДовОд');
+isPalindrome('Кекс');
